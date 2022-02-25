5d93539a0a58951ed9dcb6fb050c576f84c39ef0
BIack1st <steveminecrap69@gmail.com>
2022-02-25 13:59:34 -0600

    add init script

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,10 @@
-import flask
+import os
+import markdown
+from flask import Flask
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 @app.route('/')
 def index():
-    with open()
+    with open(os.path(app.root_path) + '/README.md', 'r') as markdown:
+        content = markdown.read()
+        return markdown.markdown(content) 
